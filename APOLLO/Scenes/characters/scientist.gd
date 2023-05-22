376a4332b7a213a2cbda290b7ff91954199b8cdd
Kyle Laverty <k.laverty@gmail.com>
2023-05-21 23:59:51 -0400

    Adding menus

--- APOLLO/Scenes/characters/scientist.gd
+++ APOLLO/Scenes/characters/scientist.gd
@@ -2,6 +2,16 @@ extends CharacterBody2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
+@export var speed = 150
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+
 func _process(delta):
 	if Input.is_action_pressed("right"):
 		_animated_sprite.play("walk_right")
