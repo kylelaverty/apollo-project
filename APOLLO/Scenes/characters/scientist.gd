8513466a72b7ab0dfbfad9e76166e4f75bb44606
Kyle Laverty <k.laverty@gmail.com>
2023-05-21 22:51:37 -0400

    Add scientist animation

--- APOLLO/Scenes/characters/scientist.gd
+++ APOLLO/Scenes/characters/scientist.gd
@@ -1,11 +1,15 @@
-extends Node2D
+extends CharacterBody2D
 
+@onready var _animated_sprite = $AnimatedSprite2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("right"):
+		_animated_sprite.play("walk_right")
+	elif Input.is_action_pressed("left"):
+		_animated_sprite.play("walk_left")
+	elif Input.is_action_pressed("down"):
+		_animated_sprite.play("walk_down")
+	elif Input.is_action_pressed("up"):
+		_animated_sprite.play("walk_up")
+	else:
+		_animated_sprite.stop()
