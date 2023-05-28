2b21e4b9cce53efb58bc53f6e41dd2cb07ae95a6
Kyle Laverty <k.laverty@gmail.com>
2023-05-28 00:37:01 -0400

    Added music

--- APOLLO/Scenes/ScienceLab.gd
+++ APOLLO/Scenes/ScienceLab.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @onready var scientist: CharacterBody2D = $scientist
 @onready var inventory_interface: Control = $UI/InventoryInterface
+@onready var background_music = $BackgroundMusic
+
 
 func _ready() -> void:
 	inventory_interface.set_player_inventory_data(scientist.inventory_data)
+	background_music.play()
